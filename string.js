d7b1d64c6e9ccfb5de3f29d068cbc780b0943db3
Sultanova Karina <karinarafaelevna@gmail.com>
2024-08-26 19:22:58 +0300

    feat:added the functionality of converting a number into a string.

---
+++ string.js
@@ -0,0 +1,10 @@
+
+function numToString(num) {
+return num.toString();
+};
+
+console.log (numToString(123));
+console.log (numToString(999));
+console.log (numToString(-100));
+
+console.log (typeof numToString(123));
