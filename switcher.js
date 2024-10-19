0e946e8c539ce106c3be6c430547f52352b9acb1
Sultanova Karina <karinarafaelevna@gmail.com>
2024-10-19 17:40:54 +0300

    task 31

--- switcher.js
+++ switcher.js
@@ -1,7 +1,6 @@
 function switcher(x){
-    const alphabet = ['z', 'y', 'x', 'w', 'v', 'u', 't', 
-                         's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 
-                         'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a','!', '?',' '];
+    const alphabet = ['z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 
+'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a','!', '?',' '];
       return x.map(num=>alphabet[Number(num)-1]).join('');
     }
     console.log (switcher(['24', '12', '23', '22', '4', '26', '9', '8']));
